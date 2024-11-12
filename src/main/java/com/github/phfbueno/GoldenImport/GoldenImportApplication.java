18352e240922b56786b6e4097e5597c39936704d
Paulo Bueno <phf.bueno@gmail.com>
2024-11-11 23:09:39 -0300

    arquivos faltante

---
+++ src/main/java/com/github/phfbueno/GoldenImport/GoldenImportApplication.java
@@ -0,0 +1,13 @@
+package com.github.phfbueno.GoldenImport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenImportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/phfbueno/GoldenImport/GoldenImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.phfbueno.GoldenImport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
